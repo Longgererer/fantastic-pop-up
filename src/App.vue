53bd0d44a2c35e9d8be1671d8729461b102ff190
xiaolong Hao <2072451919@qq.com>
2019-05-13 20:44:31 +0800

    accomplish

---
+++ src/App.vue
@@ -0,0 +1,139 @@
+<template>
+  <div id="app">
+    <h2>弹窗</h2>
+    <button id="openPop" @click="openPop">normal-pop</button><br />
+    <button id="confirm" @click="openConfirm">confirm</button><br />
+    <h2>提示</h2>
+    <div class="btn-box">
+      <button id="topInfo" @click="openInfo('infoConfigTop')">
+        topInfo
+        <info :infoConfig="infoConfigTop" type="top" /></button
+      ><br />
+      <button id="leftInfo" @click="openInfo('infoConfigLeft')">
+        leftInfo
+        <info :infoConfig="infoConfigLeft" type="left" />
+      </button>
+      <button id="rightInfo" @click="openInfo('infoConfigRight')">
+        rightInfo
+        <info :infoConfig="infoConfigRight" type="right" /></button
+      ><br />
+      <button id="bottomInfo" @click="openInfo('infoConfigBottom')">
+        bottomInfo
+        <info :infoConfig="infoConfigBottom" type="bottom" />
+      </button>
+    </div>
+    <popUp :pop="pop">
+      <h3>我是标题</h3>
+      <p>
+        这里是内容，这里是内容，这里是内容，这里是内容，这里是内容，这里是内容，这里是内容，这里是内容
+      </p>
+    </popUp>
+    <confirm :confirm="confirm">
+      <p>这里是提示，这里是提示，这里是提示</p>
+    </confirm>
+  </div>
+</template>
+<script>
+import popUp from './components/popUp'
+import confirm from './components/confirm'
+import info from './components/info'
+export default {
+  name: 'App',
+  data() {
+    return {
+      pop: {
+        isShow: false
+      },
+      confirm: {
+        isShow: false
+      },
+      infoConfigTop: {
+        isShow: false,
+        time: 3000,
+        content: 'this is top info',
+        background: 'rgba(0, 0, 0, .7)',
+        color: '#fff'
+      },
+      infoConfigLeft: {
+        isShow: false,
+        time: 3000,
+        content: 'this is left info',
+        background: 'rgba(0, 0, 0, .7)',
+        color: '#fff'
+      },
+      infoConfigRight: {
+        isShow: false,
+        time: 3000,
+        content: 'this is right info',
+        background: 'rgba(0, 0, 0, .7)',
+        color: '#fff'
+      },
+      infoConfigBottom: {
+        isShow: false,
+        time: 3000,
+        content: 'this is bottom info',
+        background: 'rgba(0, 0, 0, .7)',
+        color: '#fff'
+      }
+    }
+  },
+  components: {
+    popUp,
+    confirm,
+    info
+  },
+  methods: {
+    openPop() {
+      this.pop.isShow = true
+    },
+    openConfirm() {
+      this.confirm.isShow = true
+    },
+    openInfo(obj) {
+      console.log(obj)
+      this[obj].isShow = true
+    }
+  }
+}
+</script>
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+#openPop,
+#confirm,
+#topInfo,
+#leftInfo,
+#bottomInfo,
+#rightInfo {
+  margin-top: 10px;
+  color: #fff;
+  background-color: #ffd700;
+  border: none;
+  padding: 8px;
+  border-radius: 5px;
+}
+#topInfo,
+#leftInfo,
+#bottomInfo,
+#rightInfo {
+  outline: none;
+  position: relative;
+}
+#openPop:hover,
+#confirm:hover,
+#topInfo:hover,
+#leftInfo:hover,
+#bottomInfo:hover,
+#rightInfo:hover {
+  background-color: #dab902;
+}
+.btn-box {
+  margin: 50px 0;
+}
+</style>
---
+++ src/components/confirm.vue
@@ -0,0 +1,123 @@
+<template>
+  <transition name="slide">
+    <div class="confirm" v-if="confirm.isShow">
+      <div class="bg"></div>
+      <div class="confirm-content">
+        <div class="content-head">
+          <i @click="close"></i>
+        </div>
+        <div class="content">
+          <div class="content-text">
+            <slot></slot>
+          </div>
+        </div>
+        <div class="content-foot">
+          <button class="foot-btn" @click="close">确定</button>
+          <button class="foot-btn" @click="close">取消</button>
+        </div>
+      </div>
+    </div>
+  </transition>
+</template>
+<script>
+export default {
+  name: 'confirm',
+  props: {
+    confirm: Object
+  },
+  data() {
+    return {
+      isShow: true,
+      time: 3000,
+      content: '',
+      direction: 'right',
+      background: '',
+      color: '#fff'
+    }
+  },
+  methods: {
+    close() {
+      this.confirm.isShow = false
+    }
+  }
+}
+</script>
+<style scoped>
+.slide-enter-active,
+.slide-leave-active {
+  transition: opacity 0.3s;
+}
+.slide-enter,
+.slide-leave-to {
+  opacity: 0;
+}
+.confirm {
+  position: fixed;
+  left: 0;
+  top: 0;
+  z-index: 999999;
+  width: 100%;
+  height: 100%;
+}
+.bg {
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.2);
+}
+.confirm-content {
+  display: flex;
+  flex-direction: column;
+  width: 320px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 1000000;
+  box-shadow: 0px 1px 3px 0px #999999;
+  border-radius: 10px;
+}
+.content-head {
+  position: relative;
+  width: 100%;
+  height: 25px;
+  background-color: #048754;
+  border-top-left-radius: 10px;
+  border-top-right-radius: 10px;
+}
+.content-head i:before {
+  position: absolute;
+  right: 15px;
+  color: #fff;
+  margin-left: 5px;
+  font-style: normal;
+  content: '\2716';
+}
+.content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  background-color: #fff;
+  box-sizing: border-box;
+}
+.content-text {
+  width: 90%;
+}
+.content-foot {
+  padding: 10px 0;
+  width: 100%;
+  background-color: #fff;
+  border-bottom-right-radius: 10px;
+  border-bottom-left-radius: 10px;
+}
+.foot-btn {
+  color: #fff;
+  border: none;
+  background-color: #048754;
+  border-radius: 5px;
+  padding: 5px 10px;
+  margin-right: 10px;
+}
+</style>
+
